c5e8086ef69142ac21ff70e2532589cfe5ded4fc
harshithbhaskar69 <harshithsuomi@gmail.com>
2024-08-09 19:28:53 +0300

    fixed dependencies

--- src/index.ts
+++ src/index.ts
@@ -1,37 +1,41 @@
-import express, {Express, Request, Response} from 'express'
-import { PORT } from './secrets'
-import rootRouter from './routes'
-import { PrismaClient } from '@prisma/client'
-import { errorMiddleware } from './middlewares/errors'
-import { SignUpSchema } from './schema/users'
+import express, { Express, Request, Response } from "express";
+import { PORT } from "./secrets";
+import rootRouter from "./routes";
+import { PrismaClient } from "@prisma/client";
+import { errorMiddleware } from "./middlewares/errors";
+import { SignUpSchema } from "./schema/users";
+import { authMiddleware } from "./middlewares/auth";
 
-const app:Express  = express()
+const app: Express = express();
 
-app.use(express.json())
+app.use(express.json());
 
-app.use('/api', rootRouter);
+app.use("/api", rootRouter);
 
 export const prismaCilent = new PrismaClient({
-    log:['query']
+  log: ["query"],
 }).$extends({
-    result:{
-        address:{
-            formattedAddress: {
-                needs: {
-                    lineOne: true,
-                    lineTwo: true,
-                    city: true,
-                    country: true,
-                    pincode: true
-                },
-                compute: (addr) => {
-                    return `${addr.lineOne}, ${addr.lineTwo}, ${addr.city}, ${addr.country}-${addr.pincode}`
-                }
-            }
-        }
-    }
-})
+  result: {
+    address: {
+      formattedAddress: {
+        needs: {
+          lineOne: true,
+          lineTwo: true,
+          city: true,
+          country: true,
+          pincode: true,
+        },
+        compute: (addr) => {
+          return `${addr.lineOne}, ${addr.lineTwo}, ${addr.city}, ${addr.country}-${addr.pincode}`;
+        },
+      },
+    },
+  },
+});
 
-app.use(errorMiddleware)
+app.use(errorMiddleware);
+app.use(authMiddleware);
 
-app.listen(PORT, () => {console.log('App working!')})
+app.listen(PORT, () => {
+  console.log("App working!");
+});
